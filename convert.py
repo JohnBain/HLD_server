62327894631d498785cbd7b566bc0bb8d55b8375
JohnBain <JohnBain@users.noreply.github.com>
2023-08-21 16:56:53 -0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,11 +2,9 @@ import os
 from flask import Flask, flash, request, redirect, url_for, send_file, render_template
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
-# from rembg import remove
+from rembg import remove, new_session
 from PIL import Image
-
-def remove(file):
-    return file
+from separate import most_interesting_third
 
 UPLOAD_FOLDER = 'PYTHON_UPLOADS'
 TRANSFORM_FOLDER = "TRANSFORMS"
@@ -51,12 +49,21 @@ def upload_file():
 
 def remove_bg(filename):
     input_path = os.path.join(app.config['UPLOAD_FOLDER'] + "/") + filename
-    output_path = os.path.join(app.config['TRANSFORM_FOLDER'] + "/" + filename)
+    output_path = os.path.join(app.config['TRANSFORM_FOLDER'] + "/" + filename + ".png")
+    print(f"input path: {input_path}")
+    print(f"output path: {output_path}")
     input = Image.open(input_path)
-    output = remove(input)
-    output.save(output_path)
-    print('output?')
+    session = new_session('u2net_cloth_seg')
+    output = remove(input, 
+        session=session, 
+        alpha_matting=True, 
+        alpha_matting_foreground_threshold=190,
+        alpha_matting_background_threshold=4,
+        alpha_matting_erode_size=14,
+        )
     print(output)
+    output = most_interesting_third(output)
+    output.save(output_path)
     return output_path
 
 @app.route('/download', methods=['GET']) #web interface
@@ -70,10 +77,15 @@ def download_file():
 @app.route('/transform', methods=['POST']) #direct POST request
 def transform():
     print('request data?')    
+    print(request)
+    for i in request.files:
+        print(i)
     file = FileStorage(request.files['data'])
     filename = request.files['data'].filename
     print(filename)
     temp_path = os.path.join(app.config['UPLOAD_FOLDER'] + "/") + filename
+    print('temp path:')
+    print(temp_path)
     file.save(temp_path)
     final_file = remove_bg(filename)
     return send_file(final_file, as_attachment=True)
