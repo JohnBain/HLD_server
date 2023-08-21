6ad70a99cc145ea18ff8ea2c7ffb6b566a36f441
John Bain <jbainschool@gmail.com>
2023-08-21 16:27:52 -0400

    adding separate.py for most interesting third

---
+++ separate.py
@@ -0,0 +1,46 @@
+from PIL import Image
+
+test_image = "woman_shirt.png"
+
+SEPARATE_INTO = 3
+
+def determine_winner(lis):
+	# approximate the most filled-out third by figuring out the pane with the most colored pixels
+	# this will probably have to be replaced later down the line
+
+	new_list = []
+	third_len = int(len(lis)/SEPARATE_INTO)
+	print(third_len)
+	for i in range(0,SEPARATE_INTO):
+		total = 0
+		for y in range(third_len*i,third_len*(i+1)):
+			total += lis[y]
+		new_list.append(total)
+	for i in range(0,len(new_list)):
+		winner = 0
+		if (new_list[i] > winner):
+			winner = i
+
+	return winner+1 #turns 0,1,2 into 1,2,3
+
+
+def most_interesting_third(orig):
+	ims = {}
+	with Image.open(orig) as im:
+		width = im.width
+		height = im.height
+		print(f"{height}x{width}")
+		bs = im.tobytes()
+		winner = determine_winner(bs)
+
+		third_height = int(height/SEPARATE_INTO)
+		cropped = im.crop((0,
+			third_height*(winner-1),
+			width,
+			third_height*winner)) 
+		#left,upper,right,lower 
+		#cropped.show()
+		return cropped
+		
+
+most_interesting_third(test_image)
