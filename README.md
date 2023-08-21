6732cf06f9155611f5c57d816fd2485d6dbb2d15
John Bain <jbainschool@gmail.com>
2023-08-20 21:26:33 -0400

    f

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HLD_server
+
+curl -X POST -F "data=@1200x0.jpg" http://localhost:97/curl_upload     
--- convert.py
+++ convert.py
@@ -1,16 +1,28 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, send_file, render_template
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+# from rembg import remove
+from PIL import Image
+
+def remove(file):
+    return file
 
 UPLOAD_FOLDER = 'PYTHON_UPLOADS'
+TRANSFORM_FOLDER = "TRANSFORMS"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TRANSFORM_FOLDER'] = TRANSFORM_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
+
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
 @app.route('/', methods=['GET'])
 def index():
@@ -37,11 +49,35 @@ def upload_file():
             return redirect(url_for('download_file',filename=filename),code=301 )#+ f"?=filename={filename}", code=301)
             # redirect(url_for('download_file'), filename=filename)
 
+def remove_bg(filename):
+    input_path = os.path.join(app.config['UPLOAD_FOLDER'] + "/") + filename
+    output_path = os.path.join(app.config['TRANSFORM_FOLDER'] + "/" + filename)
+    input = Image.open(input_path)
+    output = remove(input)
+    output.save(output_path)
+    print('output?')
+    print(output)
+    return output_path
 
-@app.route('/download', methods=['GET'])
+@app.route('/download', methods=['GET']) #web interface
 def download_file():
     print(f"diagnostic info. {request.args.get('filename')}")
-    return send_file(f"{UPLOAD_FOLDER}/{request.args.get('filename')}", as_attachment=True)
+    temp_path = f"{UPLOAD_FOLDER}/{request.args.get('filename')}"
+    final_path = remove_bg(temp_path)
+    return send_file(final_path, as_attachment=True)
     #return "Hello world"
+
+@app.route('/transform', methods=['POST']) #direct POST request
+def transform():
+    print('request data?')    
+    file = FileStorage(request.files['data'])
+    filename = request.files['data'].filename
+    print(filename)
+    temp_path = os.path.join(app.config['UPLOAD_FOLDER'] + "/") + filename
+    file.save(temp_path)
+    final_file = remove_bg(filename)
+    return send_file(final_file, as_attachment=True)
+
+
 
 app.run('0.0.0.0', 97, use_reloader=True)
