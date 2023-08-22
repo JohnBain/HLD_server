d894c6f3f70d690a37a115a00e336ba2785e2741
John Bain <jbainschool@gmail.com>
2023-08-21 20:12:30 -0400

    Improved algorithm for picking most interesting

--- separate.py
+++ separate.py
@@ -1,12 +1,14 @@
 from PIL import Image
 
-test_image = "woman_shirt.png"
-
+TEST = True
 SEPARATE_INTO = 3
+
+if TEST:
+	test_image = "shirtpants2.png"
+
 
 def determine_winner(lis):
 	# approximate the most filled-out third by figuring out the pane with the most colored pixels
-	# this will probably have to be replaced later down the line
 
 	new_list = []
 	third_len = int(len(lis)/SEPARATE_INTO)
@@ -14,33 +16,50 @@ def determine_winner(lis):
 	for i in range(0,SEPARATE_INTO):
 		total = 0
 		for y in range(third_len*i,third_len*(i+1)):
-			total += lis[y]
+			if (lis[y][0] + lis[y][1] + lis[y][2]) > 30:
+				total += 1
+			
 		new_list.append(total)
-	for i in range(0,len(new_list)):
-		winner = 0
-		if (new_list[i] > winner):
-			winner = i
+		print(f"total {i} now: {total}")
+	print('lists:')
+	print(new_list[0])
+	print(new_list[1])
+	print(new_list[2])
+
+	winner = new_list.index(max(new_list))
+
+	print(winner)
 
 	return winner+1 #turns 0,1,2 into 1,2,3
 
 
-def most_interesting_third(orig):
-	ims = {}
-	with Image.open(orig) as im:
-		width = im.width
-		height = im.height
-		print(f"{height}x{width}")
-		bs = im.tobytes()
-		winner = determine_winner(bs)
+if TEST:
+	def most_interesting_third(orig):
+		ims = {}
+		if TEST:
+			with Image.open(orig) as im:
+				width = im.width
+				height = im.height
+				print(f"{height}x{width}")
+				bs = list(im.getdata())
+				# print("BS!")
+				# print(bs)
+				winner = determine_winner(bs)
+				print(f"winner? {winner}")
+				third_height = int(height/SEPARATE_INTO)
+				cropped = im.crop((0,
+					third_height*(winner-1),
+					width,
+					third_height*winner)) 
+				#left,upper,right,lower 
+				cropped.show()
+				return cropped
 
-		third_height = int(height/SEPARATE_INTO)
-		cropped = im.crop((0,
-			third_height*(winner-1),
-			width,
-			third_height*winner)) 
-		#left,upper,right,lower 
-		#cropped.show()
-		return cropped
+else:
+	def most_interesting_third(orig):
+		print('hello world!')
+
 		
 
-most_interesting_third(test_image)
+if TEST:
+	most_interesting_third(test_image)
